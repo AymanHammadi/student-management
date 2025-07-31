98cb41697415c9f6715b12dc55ad82329ebe3b8a
Ayman Hammadi <86633489+AymanHammadi@users.noreply.github.com>
2025-07-31 23:45:27 +0300

    clear caches before running migrations and adjust

--- start.sh
+++ start.sh
@@ -7,14 +7,19 @@ if [ ! -f /app/database/database.sqlite ]; then
     chmod 664 /app/database/database.sqlite
 fi
 
+# Clear previous caches
+echo "Clearing old caches..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
 # Run migrations
 echo "Running migrations..."
 php artisan migrate --force
 
 # Seed the database
 echo "Seeding the database..."
-php artisan migrate --force --seed
-
+php artisan db:seed --force
 
 # Cache configurations
 echo "Caching configurations..."
